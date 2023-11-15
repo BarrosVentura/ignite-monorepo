19a7750975bebf85c22e2d8537acc56aa3c2a269
Adson Ventura <adsonventura@gmail.com>
2023-11-15 15:31:55 -0300

    Adiciona controles de estilo para os componentes avatar, box, button, heading e text

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
   args: {
     src: 'https://github.com/BarrosVentura.png',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -6,8 +6,31 @@ export default {
   title: 'Typegraphy/Text',
   component: Text,
   args: {
+    size: 'md',
     children:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut dolore sequi dignissimos qui tempore corrupti, numquam dicta nostrum inventore? Quae pariatur facilis aut eos rem excepturi quo iure aliquid placeat!',
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro reprehenderit est veniam? Rem recusandae sint perspiciatis debitis corrupti. Deserunt neque exercitationem illo voluptatum laudantium culpa suscipit accusantium blanditiis voluptas numquam.',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
   },
 } as Meta<TextProps>
 
